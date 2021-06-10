65d43124e5afec687aeb792cad9ffc4fdd7bce7c
franco-giordano <francogior98@gmail.com>
2021-06-09 23:40:36 -0300

    match filter for q4

--- common/controllers/filter_matches_controller.py
+++ common/controllers/filter_matches_controller.py
@@ -1,11 +1,10 @@
 from common.encoders.batch_encoder_decoder import BatchEncoderDecoder
-from src.filter_query3 import FilterQuery3
 from common.models.sharded_outgoing_batcher import ShardedOutgoingBatcher
 from common.utils.rabbit_utils import RabbitUtils
 import logging
 
-class FilterQuery3Controller:
-    def __init__(self, rabbit_ip, matches_exchange_name, output_exchange_name, reducers_amount, routing_key, batch_size):
+class FilterMatchesController:
+    def __init__(self, rabbit_ip, matches_exchange_name, output_exchange_name, reducers_amount, routing_key, batch_size, match_filter):
         self.matches_exchange_name = matches_exchange_name
         self.output_exchange_name = output_exchange_name
 
@@ -17,7 +16,7 @@ class FilterQuery3Controller:
         # output exchange
         RabbitUtils.setup_output_direct_exchange(self.channel, self.output_exchange_name)
 
-        self.filter = FilterQuery3()
+        self.filter = match_filter
         self.sharded_outgoing_batcher = ShardedOutgoingBatcher(self.channel, reducers_amount, batch_size, output_exchange_name, tkn_key='token')
 
     def run(self):
--- filter-matches-q4/main.py
+++ filter-matches-q4/main.py
@@ -1,6 +1,6 @@
 from common.utils.config_setup import setup
 from common.controllers.filter_matches_controller import FilterMatchesController
-from src.filter_query3 import FilterQuery3
+from src.filter_query4 import FilterQuery4
 
 def main():
 	config_params = setup('config.ini',
@@ -9,15 +9,15 @@ def main():
 		'RABBIT_IP': False,
 		'NEXT_REDUCERS_AMOUNT': True,
 		'BATCH_SIZE': True,
-		'1v1_ROUTING_KEY': False})
+		'TEAM_ROUTING_KEY': False})
 	rabbit_ip = config_params['RABBIT_IP']
 	matches_exchange_name = config_params['MATCHES_BY_LADDER_EXCHANGE_NAME']
 	output_exchange_name = config_params['OUTPUT_EXCHANGE_NAME']
 	reducers_amount = config_params['NEXT_REDUCERS_AMOUNT']
-	input_routing_key = config_params['1v1_ROUTING_KEY']
+	input_routing_key = config_params['TEAM_ROUTING_KEY']
 	batch_size = config_params['BATCH_SIZE']
 
-	match_filter = FilterQuery3()
+	match_filter = FilterQuery4()
 	controller = FilterMatchesController(rabbit_ip, matches_exchange_name, \
 		output_exchange_name, reducers_amount, input_routing_key, batch_size, match_filter)
 	controller.run()
